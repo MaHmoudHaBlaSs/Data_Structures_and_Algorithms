f262f9f323c1c9fa7df6ebbced537f50188a5b3d
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2025-02-03 20:14:13 +0200

    Update FindPeakElement.java

--- Binary Search/FindPeakElement.java
+++ Binary Search/FindPeakElement.java
@@ -1,23 +1,23 @@
 // https://leetcode.com/problems/find-peak-element/description/
+
 class Solution {
     public int findPeakElement(int[] nums) {
         int left = 0, right = nums.length-1;
-
+        
         while (left <= right){
             int mid = (left+right)/2;
 
-            if ((left == right) || (mid-1 == -1))
+            if (left == mid)
                 return (nums[left] >= nums[right])?left:right;
-            if (nums[mid] > nums[mid-1]){
-                if (nums[mid] > nums[mid+1])
-                    return mid;
-                else // val of (mid+1) > val of (mid) 
-                    left = mid+1;
-            } else {
-                right = mid-1;
-            } 
+
+            if (nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1])
+                return mid;
+            else if (nums[mid] > nums[mid-1])
+                left = mid;
+            else 
+                right = mid;
         }
-        // Arbitary return statement, it will never reached.
+        // Arbitary Return Statement, Will never reached for valid inputs.
         return -1;
     }
 }
