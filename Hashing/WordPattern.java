28f1ea8ef639364103159accfbf2d209e430bc46
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2025-02-04 21:23:12 +0200

    Update WordPattern.java

--- Hashing/WordPattern.java
+++ Hashing/WordPattern.java
@@ -1,33 +1,23 @@
-// https://leetcode.com/problems/word-pattern/
-
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         HashMap<Character, String> map = new HashMap<>();
-        int i = 0, j = 0;
-        for (;i < pattern.length() && j < s.length(); i++, j++){
-            StringBuilder builder = new StringBuilder();
 
-            while(s.charAt(j) != ' ' ){
-                builder.append(s.charAt(j));
-                if (j == s.length()-1)
-                    break;
+        int i,j;
+        for (i = 0, j = 0; i < pattern.length(); i++, j++){
+            StringBuilder str = new StringBuilder();
+            while (j < s.length() && s.charAt(j) != ' '){
+                str.append(s.charAt(j));
                 j++;
             }
+            String word = str.toString();
 
-            String word = builder.toString();
             if (map.containsKey(pattern.charAt(i))){
-                if (!map.get(pattern.charAt(i)).equals(word))
-                    return false;
-            } else {
-                if (map.containsValue(word))
-                    return false;
-                else
-                    map.put(pattern.charAt(i), word);
+                if (!map.get(pattern.charAt(i)).equals(word)) return false;
+            }else{
+                if (map.containsValue(word)) return false;
+                else map.put(pattern.charAt(i), word);
             }
         }
-        if ((i == pattern.length()) && (j == s.length()))
-            return true;
-        else
-            return false;
+        return i == pattern.length() && j == s.length()+1;
     }
 }
