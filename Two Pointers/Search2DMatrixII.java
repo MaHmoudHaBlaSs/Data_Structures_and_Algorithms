abd4eb6e09e0261ef378d19004873323f6e69312
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2025-02-07 21:35:53 +0200

    Create Search2DMatrixII.java

---
+++ Two Pointers/Search2DMatrixII.java
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+
+class Solution {
+
+    // T: O(n+m) >> O(n)
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0, j = matrix[0].length-1;
+        while (i < matrix.length && j >= 0){
+            int currVal = matrix[i][j];
+            if (currVal > target)
+                j--;
+            else if (currVal < target)
+                i++;
+            else 
+                return true;
+        }
+        return false;
+    }
+  
+    // Binary Search Solution >> T: O(nLog(m))
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int cols = matrix[0].length;
+        for (int i = 0; i < matrix.length; i++){
+            int left = 0, right = cols-1;
+            
+            while (left <= right){
+                int mid = (left+right)/2;
+
+                if (matrix[i][mid] > target)
+                    right = mid-1;
+                else if (matrix[i][mid] < target)
+                    left = mid+1;
+                else 
+                    return true;
+            }
+        }
+        return false;
+    }
+}
