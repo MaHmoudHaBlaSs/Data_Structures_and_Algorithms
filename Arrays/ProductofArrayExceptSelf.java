2782b12999af3a2e920a0831fadb46e505d8608e
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2025-02-08 23:49:50 +0200

    Update ProductofArrayExceptSelf.java

--- Arrays/ProductofArrayExceptSelf.java
+++ Arrays/ProductofArrayExceptSelf.java
@@ -1,15 +1,21 @@
 //  https://leetcode.com/problems/product-of-array-except-self/description/
 
 class Solution {
+    // T: O(n)                  S: O(1) >> return isn't counted.
     public int[] productExceptSelf(int[] nums) {
-        int[] pre = new int[nums.length]; pre[0] = 1;
         int[] answer = new int[nums.length];
+        answer[0] = 1;
         int post = 1;
-        for (int i = 1; i < nums.length; i++)
-            pre[i] = pre[i-1]*nums[i-1];
-        for (int j = nums.length-1; j >= 0; j--){
-            answer[j] = post*pre[j];
-            post *= nums[j];
+
+        // Filling answer with pre-fix values.
+        for (int i = 1; i < nums.length; i++){
+            answer[i] = answer[i-1]*nums[i-1];
+        }
+        
+        // Overwriting prefixes with final answer after multiplying them by suffixes
+        for (int i = answer.length-1; i >= 0; i--){
+            post *= (i == answer.length-1)?1:nums[i+1];
+            answer[i] = answer[i]*post;
         }
         return answer;
     }
