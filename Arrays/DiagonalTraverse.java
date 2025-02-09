e4f5e68d4c186778ca6135fa7b6d276181bf8a5b
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2025-02-09 22:19:40 +0200

    Update DiagonalTraverse.java

--- Arrays/DiagonalTraverse.java
+++ Arrays/DiagonalTraverse.java
@@ -3,37 +3,33 @@
 class Solution {
 public  int[] findDiagonalOrder(int[][] mat) {
         int rows = mat.length, cols = mat[0].length;
-        int i = 0, j = 0, arr[] = new int[rows*cols];
-        int ind = 0;
+        int i = 0, j = 0, ind = 0;
+        int[] arr = new int[rows*cols];
         boolean goingUp = true;
         // We will control i and j based on direction Up or Down
         // Initially set to Up.
 
         while (i < rows && j < cols){
-            if (goingUp){
-                arr[ind] = mat[i][j];
-                i--;
-                j++;
-
-                if (i < 0 && j < cols){
+            arr[ind] = mat[i][j];
+            
+            if (goingUp) { // Up
+                i--; j++;
+                if (i == -1 && j < cols){
                     i++;
                     goingUp = false;
-                } else if (j >= cols){
+                }else if (j == cols){
                     i += 2;
                     j--;
                     goingUp = false;
                 }
-            }else{ // Going Down
-                arr[ind] = mat[i][j];
-                i++;
-                j--;
-
-                if (j < 0 && i < rows){
+            } else { // Down
+                i++; j--;
+                if (j == -1 && i < rows){
                     j++;
                     goingUp = true;
-                } else if (i >= rows){
-                    j += 2;
+                } else if (i == rows){
                     i--;
+                    j += 2;
                     goingUp = true;
                 }
             }
