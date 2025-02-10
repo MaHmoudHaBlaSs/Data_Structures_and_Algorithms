f0afbd232b945d9fc4c2997a3cf867354d1eef8a
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2025-02-10 20:41:18 +0200

    Update FindPeakElement.java

--- Binary Search/FindPeakElement.java
+++ Binary Search/FindPeakElement.java
@@ -3,19 +3,19 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         int left = 0, right = nums.length-1;
-        
+
         while (left <= right){
             int mid = (left+right)/2;
 
             if (left == mid)
-                return (nums[left] >= nums[right])?left:right;
-
-            if (nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1])
-                return mid;
-            else if (nums[mid] > nums[mid-1])
-                left = mid;
-            else 
-                right = mid;
+                return (nums[left] > nums[right])?left:right;
+            if ((nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]))
+                return mid;  
+            if (nums[mid-1] > nums[mid]){
+                right = mid-1;
+            } else {
+                left = mid+1;
+            }
         }
         // Arbitary Return Statement, Will never reached for valid inputs.
         return -1;
