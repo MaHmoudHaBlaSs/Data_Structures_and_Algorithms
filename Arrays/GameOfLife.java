b626090ae5863ac1410ea9ad611812a79c42a2c0
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2025-02-11 19:37:02 +0200

    Update GameOfLife.java

--- Arrays/GameOfLife.java
+++ Arrays/GameOfLife.java
@@ -4,42 +4,37 @@ class Solution {
     // T: O(n2)
     public void gameOfLife(int[][] board) {
         int m = board.length, n = board[0].length;
-        int[][] next = new int[m][n];
-        
+        int[][] lives = new int[m][n];
+
         // T: O(n2)
         for (int i = 0; i < m; i++){
             for (int j = 0; j < n; j++){
-                int lives = getLives(board, m, n, i, j);
-
-                if (board[i][j] == 1){
-                    if (lives < 2 || lives > 3)
-                        next[i][j] = 0;
-                    else 
-                        next[i][j] = 1;
-                } else {
-                    if (lives == 3)
-                        next[i][j] = 1;
-                }
+                lives[i][j] = getLives(board, m, n, i, j);
             }
         }
+
         // T: O(n2)
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                board[i][j] = next[i][j]; 
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (board[i][j] == 1){
+                    if (lives[i][j] < 2 || lives[i][j] > 3)
+                        board[i][j] = 0;
+                }else{
+                    if (lives[i][j] == 3)
+                        board[i][j] = 1;
+                }
+                    
             }
         }
     }
-    
     // O(8) >> O(1)
     public int getLives(int[][] board, int m, int n, int x, int y){
-        int lives = (board[x][y] == 1)?-1:0;
-        int rows = x+2, cols = y+2;
-
-        for (int i = x-1;i < rows && i < m; i++){
-            if (i < 0)
+        int lives = (board[x][y] == 0)?0:-1;
+        for (int i = x-1; i < x+2; i++){
+            if (i == -1 || i == m)
                 continue;
-            for (int j = y-1; j < cols && j < n; j++){
-                if (j < 0)
+            for (int j = y-1; j < y+2; j++){
+                if (j == -1 || j == n)
                     continue;
                 lives += board[i][j];
             }
