3af9147993d5f6c0a2917e1e3ed2f18af82a7d70
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2025-02-12 20:42:09 +0200

    Update MergeSortedArray.java

--- Arrays/MergeSortedArray.java
+++ Arrays/MergeSortedArray.java
@@ -4,23 +4,25 @@ class Solution {
   
     // T: O(n+m)       S: O(1)
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m-1, j = n-1, k = m+n-1;
-        while (k >= 0 && i >= 0 && j >= 0){
-            if (nums1[i] > nums2[j]){
-                nums1[k] = nums1[i];
-                i--;k--;
+        int swapper = nums1.length-1, i = m-1, j = n-1;
+
+        while (i >= 0 && j >= 0){
+            if (nums1[i] >= nums2[j]){
+                nums1[swapper] = nums1[i];
+                i--;
             }else{
-                nums1[k] = nums2[j];
-                j--;k--;
+                nums1[swapper] = nums2[j];
+                j--;
             }
+            swapper--;
         }
-        while (i == -1 && j != -1){
-            nums1[k] = nums2[j];
-            j--;k--;
+        while (j >= 0){
+            nums1[swapper] = nums2[j];
+            j--; swapper--;
         }
-        while (j == -1 && i != -1){
-            nums1[k] = nums1[i];
-            i--;k--;
+        while (i >= 0){
+            nums1[swapper] = nums1[i];
+            i--; swapper--;
         }
     }
 }
