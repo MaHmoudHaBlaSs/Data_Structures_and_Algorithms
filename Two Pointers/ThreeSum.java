20ad3dcd8f011e602af6297ac7416c52fbee19b1
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2025-02-14 23:56:02 +0200

    Update ThreeSum.java

--- Two Pointers/ThreeSum.java
+++ Two Pointers/ThreeSum.java
@@ -1,34 +1,31 @@
 // https://leetcode.com/problems/3sum/
 
 class Solution {
-    // T: O(n2) + O(nLogn(n)) >> O(n2)
+    // T: O(n2)
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums); // O(nLog(n))
         List<List<Integer>> triplets = new LinkedList<>();
+        Arrays.sort(nums);
 
-        // T: O(n*m) >> O(n2)
+        // T: O(n)
         for (int i = 0; i < nums.length; i++){
-            int target = nums[i]*-1;
             int left = i+1, right = nums.length-1;
             
-            if (i > 0 && nums[i] == nums[i-1])
-                continue;
-            if (nums[i] > 0)
-                break;
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+
+            // T: O(m)
             while (left < right){
-                int sum = nums[left]+nums[right];
-                
-                if (sum > target)
+                int sum = nums[i]+nums[left]+nums[right];
+                if (sum > 0)
                     right--;
-                else if (sum < target)
+                else if (sum < 0)
                     left++;
-                else {
-                    triplets.add(List.of(nums[i], nums[left], nums[right]));
-                    left++; right--;
-                    while (left < right && nums[left-1] == nums[left])
-                        left++;
-                    while (left < right && nums[right+1] == nums[left])
-                        right--;
+                else{
+                    triplets.add(new LinkedList(List.of(nums[i], nums[left], nums[right])));
+                    do{left++;}
+                    while(left < right && nums[left] == nums[left-1]);
+                    do{right--;}
+                    while(left < right && nums[right] == nums[right+1]);
                 }
             }
         }
