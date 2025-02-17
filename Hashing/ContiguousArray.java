8c64664be64b5a43327eaff01c0b9a311506bc79
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2025-02-17 23:27:48 +0200

    Update ContiguousArray.java

--- Hashing/ContiguousArray.java
+++ Hashing/ContiguousArray.java
@@ -1,20 +1,15 @@
 // https://leetcode.com/problems/contiguous-array/
 
 class Solution {
+    // T: O(n)      S: O(n)
     public int findMaxLength(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>(); map.put(0, -1);
-        int sum = 0, maxLen = 0, diff = 0;
-        
+        int maxArr = 0, sum = 0;
         for (int i = 0; i < nums.length; i++){
-            sum += (nums[i]==0)?-1:1;
-            
-            if (map.containsKey(sum)){
-                diff = i-map.get(sum);
-                if (diff > maxLen)
-                    maxLen = diff; 
-            }else 
-                map.put(sum, i);
+            sum += (nums[i] == 0)?-1:1;
+            if (!map.containsKey(sum)) map.put(sum, i);
+            else maxArr = Math.max(i-map.get(sum), maxArr);
         }
-        return maxLen;
+        return maxArr;
     }
 }
