5f053a29913314634eaaacac9ba8d61738bd71c9
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2025-02-18 22:12:49 +0200

    Update Search2DMatrixII.java

--- Two Pointers/Search2DMatrixII.java
+++ Two Pointers/Search2DMatrixII.java
@@ -6,13 +6,11 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int i = 0, j = matrix[0].length-1;
         while (i < matrix.length && j >= 0){
-            int currVal = matrix[i][j];
-            if (currVal > target)
+            if (matrix[i][j] == target) return true;
+            if (matrix[i][j] > target)
                 j--;
-            else if (currVal < target)
-                i++;
             else 
-                return true;
+                i++;
         }
         return false;
     }
