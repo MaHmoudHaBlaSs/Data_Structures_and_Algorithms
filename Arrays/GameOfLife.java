660f4cf45c9cdeda8b3981b563c512614ba464cb
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2025-02-18 21:57:04 +0200

    Update GameOfLife.java

--- Arrays/GameOfLife.java
+++ Arrays/GameOfLife.java
@@ -3,39 +3,33 @@
 class Solution {
     // T: O(n2)
     public void gameOfLife(int[][] board) {
-        int m = board.length, n = board[0].length;
-        int[][] lives = new int[m][n];
-
+        int rows = board.length, cols = board[0].length;
+        int[][] lives = new int[rows][cols];
+        
         // T: O(n2)
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                lives[i][j] = getLives(board, m, n, i, j);
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < cols; j++){
+                lives[i][j] = getLives(board, i, j);
             }
         }
-
         // T: O(n2)
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                if (board[i][j] == 1){
-                    if (lives[i][j] < 2 || lives[i][j] > 3)
-                        board[i][j] = 0;
-                }else{
-                    if (lives[i][j] == 3)
-                        board[i][j] = 1;
-                }
-                    
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < cols; j++){
+                if (board[i][j] == 1)
+                    board[i][j] = (lives[i][j] < 2 || lives[i][j] > 3)?0:1;
+                else
+                    board[i][j] = (lives[i][j] == 3)?1:0;
             }
         }
     }
     // O(8) >> O(1)
-    public int getLives(int[][] board, int m, int n, int x, int y){
+    public int getLives(int[][] board, int x, int y){
         int lives = (board[x][y] == 0)?0:-1;
+        
         for (int i = x-1; i < x+2; i++){
-            if (i == -1 || i == m)
-                continue;
+            if (i >= board.length || i == -1) continue;
             for (int j = y-1; j < y+2; j++){
-                if (j == -1 || j == n)
-                    continue;
+                if (j == board[0].length || j == -1) continue;
                 lives += board[i][j];
             }
         }
