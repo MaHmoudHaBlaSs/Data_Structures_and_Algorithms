641eda3989dec2b6a44f4af6fd0c44d2f329bb75
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2025-02-20 00:01:36 +0200

    Update SubtreeOfAnotherTree.java

--- Trees/SubtreeOfAnotherTree.java
+++ Trees/SubtreeOfAnotherTree.java
@@ -1,19 +1,18 @@
 // https://leetcode.com/problems/subtree-of-another-tree/description/
 
 class Solution {
-
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if (subRoot == null) return true;
         if (root == null) return false;
-        if (isSameTree(root, subRoot)) return true;
-
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        if (subRoot == null) return true;
+        if (isSametree(root, subRoot))
+            return true;
+        else 
+            return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
-    public boolean isSameTree(TreeNode root, TreeNode subRoot){
+    public boolean isSametree(TreeNode root, TreeNode subRoot){
         if (root == null && subRoot == null) return true;
-        if ((root != null && subRoot != null) && (root.val == subRoot.val)){
-            return isSameTree(root.left, subRoot.left) && isSameTree (root.right, subRoot.right);
-        }
-        return false;
+        if (root == null ^ subRoot == null) return false;
+        if (root.val != subRoot.val) return false;  
+        return isSametree(root.left, subRoot.left) && isSametree(root.right, subRoot.right);
     }
 }
