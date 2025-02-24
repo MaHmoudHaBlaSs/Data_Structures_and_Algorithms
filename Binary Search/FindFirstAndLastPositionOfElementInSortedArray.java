634e367e17baa9c6bee7905b676d89f911320352
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2025-02-24 18:37:35 +0200

    Update FindFirstAndLastPositionOfElementInSortedArray.java

--- Binary Search/FindFirstAndLastPositionOfElementInSortedArray.java
+++ Binary Search/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -1,30 +1,26 @@
 // https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+        int[] pos = {-1, -1};
         int left = 0, right = nums.length-1;
-        int[] pos = new int[2];
-
         while (left <= right){
             int mid = (left+right)/2;
-
-            if (target < nums[mid])
+            if (nums[mid] > target)
                 right = mid-1;
-            else if (target > nums[mid])
+            else if (nums[mid] < target)
                 left = mid+1;
-            else {
-                int i = mid;
-                while ((i >= 0) && (nums[i] == target))
-                    i--;
-                pos[0] = i+1; 
-                
-                i = mid;
-                while ((i <= nums.length-1) && (nums[i] == target))
-                    i++;
-                pos[1] = i-1;
-                
-                return pos;
+            else{
+                int counter = mid;
+                do
+                    pos[0] = counter;
+                while (counter-1 >= 0 && nums[counter] == nums[--counter]);
+                counter = mid;
+                do
+                    pos[1] = counter;
+                while (counter+1 < nums.length && nums[counter] == nums[++counter]);
+                break;
             }
         }
-        return new int[]{-1, -1};
+        return pos;
     }
 }
