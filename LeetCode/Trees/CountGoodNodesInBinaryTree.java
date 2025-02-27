0951dcd89331b3055ebc0cdbb2e086d2f79c3d7f
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2025-02-27 14:34:23 +0200

    Update CountGoodNodesInBinaryTree.java

--- LeetCode/Trees/CountGoodNodesInBinaryTree.java
+++ LeetCode/Trees/CountGoodNodesInBinaryTree.java
@@ -4,7 +4,11 @@ class Solution {
     public int goodNodes(TreeNode root) {
         return dfs(root, root.val);
     }
-    // T: O(n)           S:O(n) >> Check for space complexity
+    // For balanced trees (best case) space complexity would be O(Log(n)) [Height of the tree] and for worst case
+    // such that tree looks like LinkedList, the recursion would reach depth of n in the stack so the space complexity is O(n).
+    // Time complexity is O(n) at any case because we check each node just one time.
+    
+    // T: O(n)           S:O(n) >> Because of stack (recursion)
     public int dfs(TreeNode node, int max){
         if (node == null) return 0;
         if (node.val >= max){
@@ -12,5 +16,21 @@ class Solution {
         }else{
             return 0 + dfs(node.left, max) + dfs(node.right, max);
         }
+    }
+}
+
+// Another Implemention, same complexitites
+class Solution {
+    int nodes = 0;
+    public int goodNodes(TreeNode root) {
+        countGoods(root, root.val);
+        return nodes;
+    }
+    public void countGoods (TreeNode node , int max){
+        if (node == null) return;
+        // Pre-Order traverse
+        countGoods(node.left, Math.max(max, node.val));
+        if (node.val >= max) nodes++;
+        countGoods(node.right, Math.max(max, node.val));
     }
 }
