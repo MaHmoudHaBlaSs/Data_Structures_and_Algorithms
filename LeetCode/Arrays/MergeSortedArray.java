1fe84c20fa5c6f8747e76079fd7fc926375bf9c7
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2025-03-01 22:59:37 +0200

    Update MergeSortedArray.java

--- LeetCode/Arrays/MergeSortedArray.java
+++ LeetCode/Arrays/MergeSortedArray.java
@@ -4,25 +4,14 @@ class Solution {
   
     // T: O(n+m)       S: O(1)
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int swapper = nums1.length-1, i = m-1, j = n-1;
-
-        while (i >= 0 && j >= 0){
-            if (nums1[i] >= nums2[j]){
-                nums1[swapper] = nums1[i];
-                i--;
-            }else{
-                nums1[swapper] = nums2[j];
-                j--;
-            }
-            swapper--;
+        int m1 = m-1, m2 = n-1, writer = nums1.length-1;
+        while (writer > m1 && m1 >= 0 && m2 >= 0){
+            if (nums2[m2] >= nums1[m1])
+                nums1[writer--] = nums2[m2--];
+            else
+                nums1[writer--] = nums1[m1--];
         }
-        while (j >= 0){
-            nums1[swapper] = nums2[j];
-            j--; swapper--;
-        }
-        while (i >= 0){
-            nums1[swapper] = nums1[i];
-            i--; swapper--;
-        }
+        while (m1 == -1 && writer >= 0 && m2 >= 0)
+            nums1[writer--] = nums2[m2--];
     }
 }
