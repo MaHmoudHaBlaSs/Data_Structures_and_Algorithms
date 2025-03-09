8bc70c7baeb61408efdd5f080cc0a2a199196c0c
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2025-03-09 22:33:11 +0200

    Update GenerateParentheses.java

--- LeetCode/Backtracking/GenerateParentheses.java
+++ LeetCode/Backtracking/GenerateParentheses.java
@@ -1,25 +1,32 @@
 // https://leetcode.com/problems/generate-parentheses/description/
-// Try to solve it using Stack like Neetcode !!
+// Try to solve it using Stack !!
 
+// Optimal Solution.
 class Solution {
-    List<String> sets = new LinkedList<>();
+    StringBuilder comb = new StringBuilder(); // S: O(n)
+    List<String> combs = new LinkedList<>(); // S: O(1) >> Placeholder
+
     public List<String> generateParenthesis(int n) {
-        permutations(new StringBuilder(), 0, 0, n);
-        return sets;
+        recurse (n, 0, 0);
+        return combs;
     }
-    public void permutations(StringBuilder set, int opened, int closed, int total){
-        if (opened == total && closed == total){
-            sets.add(set.toString());
+
+    // Time and Space Complexities are difficult to compute but they are pretty high
+    public void recurse(int n, int opened, int closed){
+        if (closed == n && opened == n) {
+            combs.add(new String(comb.toString()));
             return;
         }
-        if (closed > opened) return;
-        if (opened < total && closed < total){
-            permutations(new StringBuilder(set.append('(')), opened+1, closed, total);
-            set.setLength(set.length()-1);
-            permutations(new StringBuilder(set.append(')')), opened, closed+1, total);
-        }
-        else if (opened == total)
-            permutations(new StringBuilder(set.append(')')), opened, closed+1, total);
 
+        if (opened < n){ // False only when opened == n
+            comb.append('(');
+            recurse(n, opened+1, closed);
+            comb.setLength(comb.length()-1);
+        }
+        if (opened > closed){
+            comb.append(')');
+            recurse(n, opened, closed+1);
+            comb.setLength(comb.length()-1);
+        }
     }
 }
