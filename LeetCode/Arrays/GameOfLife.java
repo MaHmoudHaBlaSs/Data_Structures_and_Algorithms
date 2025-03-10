ff5646da129a6d08d2071c862a19b0b4d77f1c79
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2025-03-10 12:30:07 +0200

    Update GameOfLife.java

--- LeetCode/Arrays/GameOfLife.java
+++ LeetCode/Arrays/GameOfLife.java
@@ -1,38 +1,29 @@
 // https://leetcode.com/problems/game-of-life/description/
 
 class Solution {
-    // T: O(n2)
+    // T: O(n2)          S: O(n)
     public void gameOfLife(int[][] board) {
-        int rows = board.length, cols = board[0].length;
-        int[][] lives = new int[rows][cols];
-        
-        // T: O(n2)
-        for (int i = 0; i < rows; i++){
-            for (int j = 0; j < cols; j++){
-                lives[i][j] = getLives(board, i, j);
-            }
-        }
-        // T: O(n2)
-        for (int i = 0; i < rows; i++){
-            for (int j = 0; j < cols; j++){
-                if (board[i][j] == 1)
-                    board[i][j] = (lives[i][j] < 2 || lives[i][j] > 3)?0:1;
-                else
-                    board[i][j] = (lives[i][j] == 3)?1:0;
-            }
-        }
+        boolean[][] nextGen = new boolean[board.length][board[0].length]; // S: O(n)
+
+        for (int i = 0; i < board.length; i++)
+            for (int j = 0; j < board[0].length; j++)
+                nextGen[i][j] = predict(board, i, j);
+        for (int i = 0; i < board.length; i++)
+            for (int j = 0; j < board[0].length; j++)
+                board[i][j] = (nextGen[i][j])?1:0;
     }
+
     // O(8) >> O(1)
-    public int getLives(int[][] board, int x, int y){
-        int lives = (board[x][y] == 0)?0:-1;
-        
-        for (int i = x-1; i < x+2; i++){
-            if (i >= board.length || i == -1) continue;
-            for (int j = y-1; j < y+2; j++){
-                if (j == board[0].length || j == -1) continue;
-                lives += board[i][j];
+    public boolean predict(int[][] board, int x, int y){
+        int neighbours = (board[x][y] == 1)?-1:0;
+    
+        for (int i = x-1; i <= x+1; i++){
+            if (i < 0 || i == board.length) continue;
+            for (int j = y-1; j <= y+1; j++){
+                if (j < 0 || j == board[0].length) continue;
+                neighbours += board[i][j];
             }
         }
-        return lives;
+        return (board[x][y] == 1)?!(neighbours < 2 || neighbours > 3): neighbours == 3;
     }
 }
