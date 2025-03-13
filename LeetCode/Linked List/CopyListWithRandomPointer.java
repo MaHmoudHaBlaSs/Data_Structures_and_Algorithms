ef78d977a16fd6b2c9523a787ed408b10e440a67
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2025-03-13 13:40:09 +0200

    Update CopyListWithRandomPointer.java

--- LeetCode/Linked List/CopyListWithRandomPointer.java
+++ LeetCode/Linked List/CopyListWithRandomPointer.java
@@ -1,21 +1,25 @@
 // https://leetcode.com/problems/copy-list-with-random-pointer/description/
 
+// Optimal
 class Solution {
     // T: O(n)     S: O(n)
     public Node copyRandomList(Node head) {
-        HashMap<Node, Node> map = new HashMap<>();
+        Map<Node, Node> map = new HashMap<>();
         Node nHead = new Node(0);
         Node temp = head, ntemp = nHead;
-        int counter = 0;
+
         while (temp != null){
             ntemp.next = new Node(temp.val);
             map.put(temp, ntemp.next);
-            temp = temp.next; ntemp = ntemp.next;
+            
+            temp = temp.next; 
+            ntemp = ntemp.next;
         }
-        temp = head; ntemp = nHead.next; counter = 0;
+        temp = head; ntemp = nHead.next;
         while (temp != null){
             if (temp.random != null) ntemp.random = map.get(temp.random);
-            temp = temp.next; ntemp = ntemp.next;
+            temp = temp.next; 
+            ntemp = ntemp.next;
         }
         return nHead.next;
     }
