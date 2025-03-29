02461a8eecaf2b22cc2b68901de2119693aaf58b
Abdullah Muhammad <139957345+Abdallahmoh443@users.noreply.github.com>
2025-03-29 17:35:14 +0200

    جانا العيد ؟

---
+++ LeetCode/Two Pointers/FindFirstPalindromicStringInTheArray.java
@@ -0,0 +1,23 @@
+https://leetcode.com/problems/find-first-palindromic-string-in-the-array/description/?envType=problem-list-v2&envId=two-pointers
+
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for (String word : words) {
+            if (isPalindrome(word)) {
+                return word;
+            }
+        }
+        return "";
+    }
+    public boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
