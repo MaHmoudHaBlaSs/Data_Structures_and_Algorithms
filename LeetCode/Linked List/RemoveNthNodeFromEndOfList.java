6c9a91c23bd744a8deed0cdc0748bca20f928f2e
Abdullah Muhammad <139957345+Abdallahmoh443@users.noreply.github.com>
2025-04-15 13:07:17 +0200

    Another  Sol

--- LeetCode/Linked List/RemoveNthNodeFromEndOfList.java
+++ LeetCode/Linked List/RemoveNthNodeFromEndOfList.java
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
 
+
+
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         int size = 0;
@@ -16,5 +18,30 @@ class Solution {
         }
         prev.next = curr.next;
         return head;
+    }
+}
+
+
+// another sol using Floyd's Slow And Fast Pointers which is take better time complexity 
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode slow = head , fast = head  ;
+
+        for(int i = 0 ; i < n ; i++ ){
+            fast = fast.next ;
+        }
+        
+        if (fast == null) {
+            return head.next;
+        }
+
+        while(fast.next != null ){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return head ;
     }
 }
